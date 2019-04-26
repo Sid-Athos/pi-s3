0d926c54771bc4f654cc35149128259a850feeaa
Sid-Athos <sa.bennaceur@gmail.com>
2019-04-26 09:39:16 +0200

    initialization

---
+++ LastBastion/LastBastion.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LastBastion.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
