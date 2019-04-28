395b75041a549963dcb28b43d009760a6632fcb7
Sid-Athos <sa.bennaceur@gmail.com>
2019-04-28 20:51:08 +0200

    fix git issues

---
+++ LastBastion/LastBastion.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using LastBastion.Model;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
