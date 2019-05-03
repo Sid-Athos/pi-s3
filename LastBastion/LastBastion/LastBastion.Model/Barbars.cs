db1d5ca4897739370c0df3090b0a178fed680105
Fopodir <abidi.feras@hotmail.com>
2019-05-03 09:32:06 +0200

    branch map

---
+++ LastBastion/LastBastion/LastBastion.Model/Barbars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastBastion.Model
+{
+    public class Barbars : Units
+    {
+        static uint _count;
+
+        public Barbars(bool flying, float posX, float posY,
+            string job, uint lifePoints, uint dmg, uint armor, bool isMoving,
+            uint attackCooldown, float speed)
+            :base(flying, posX, posY,
+            job, lifePoints, dmg, armor, isMoving,
+            attackCooldown, speed)
+        {
+            _count++;
+        }
+
+
+        /**public void Move(double x, double Y)
+        {
+            _isMoving = !isMoving;
+        }*/
+    }
+}
---
+++ LastBastion/LastBastion/LastBastion.Model/Towers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LastBastion.Model
+{
+    public class Towers : Buildings
+    {
+        
+        Villagers [] _slots;
+        uint _rank = 1;
+
+        public Towers(float posX,
+        float posY,
+        uint lifePoints,
+        uint dmg,
+        uint armor,
+        uint aaCooldown,
+        uint rank)
+            :base( posX,
+         posY,
+         lifePoints,
+         dmg,
+         armor,
+         aaCooldown,
+         rank)
+        {
+            _slots = new Villagers[2];
+        }
+
+        public uint Rank => _rank;
+
+        public int AvailableSlots => _slots.Length;
+
+        public void Upgrade()
+        {
+            if (_rank < 3)
+            {
+                _rank++;
+                Villagers[] newSlots = new Villagers[_slots.Length * 2];
+                for (int i = 0; i < _slots.Length; i++)
+                {
+                    newSlots[i] = _slots[i];
+                }
+
+                IncreaseArmor();
+                IncDamage();
+                IncHealth();
+                _slots = newSlots;
+            }
+        }
+
+        public void AddArcher(Villagers u)
+        {
+            if(!u.IsInTower)
+            {
+                _slots.SetValue(u,ShowArchers());
+                u.JoinTower(); 
+            }
+        }
+
+        public uint ShowArchers()
+        {
+            uint compteur = 0;
+
+            for (int i = 0; i < _slots.Length; i++)
+            {
+                if (_slots[i] != null )
+                {
+                    compteur++;
+                }
+            }
+
+            return compteur;
+        }
+    }
+}
